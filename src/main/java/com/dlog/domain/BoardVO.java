6e452695dc6ea7b6f4061fb20ccbc29bd6a871cd
dongjuny <dong64553@naver.com>
2020-09-03 17:49:58 +0900

    # lombok 라이브러리 의존성 추가 # BoardVO 객체 생성자,getter,setter 삭제 후 @Data 추가

--- src/main/java/com/dlog/domain/BoardVO.java
+++ src/main/java/com/dlog/domain/BoardVO.java
@@ -1,47 +1,10 @@
 package com.dlog.domain;
 
-import org.codehaus.jackson.annotate.JsonCreator;
+import lombok.Data;
 
+@Data
 public class BoardVO {
     private int board_id;
     private String board_title;
     private String board_contents;
-
-    public BoardVO(){
-        super();
-    }
-    public BoardVO(int board_id, String board_title, String board_contents) {
-        this.board_id = board_id;
-        this.board_title = board_title;
-        this.board_contents = board_contents;
-    }
-
-    @JsonCreator
-    public BoardVO(String board_title, String board_contents) {
-        this.board_title = board_title;
-        this.board_contents = board_contents;
-    }
-    public int getBoard_id() {
-        return board_id;
-    }
-
-    public String getBoard_title() {
-        return board_title;
-    }
-
-    public String getBoard_contents() {
-        return board_contents;
-    }
-
-    public void setBoard_id(int board_id) {
-        this.board_id = board_id;
-    }
-
-    public void setBoard_title(String board_title) {
-        this.board_title = board_title;
-    }
-
-    public void setBoard_contents(String board_contents) {
-        this.board_contents = board_contents;
-    }
 }
